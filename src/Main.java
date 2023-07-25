72c81c52762be51154c4dc868c445978eb5e260b
Pedro Vitor <pedrovitorcoelho@outlook.com>
2023-07-25 19:17:07 -0300

    finalizando sobre tipos de variáveis primitivas em java

--- src/Main.java
+++ src/Main.java
@@ -8,5 +8,18 @@ public class Main {
 
         // Utilizando sout, o sistema já traz o System.outprintln();
         System.out.println("Hello word");
+
+        //Em java temos dois grupos de variáveis: primitivas e não primitivos.
+
+        //Variáveis primitivas;
+
+        //byte vai de -128 a 127
+        //char 0 a 65535
+        //int -2b a 2b
+        //long -9t a 9t
+
+        byte varivaelByte = -128;
+
+        System.out.println(varivaelByte);
     }
 }
