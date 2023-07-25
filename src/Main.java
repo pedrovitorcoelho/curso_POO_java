ea2e595222c694e082a6612565bd8355dc93c80a
Pedro Vitor <pedrovitorcoelho@outlook.com>
2023-07-24 22:04:04 -0300

    primeiros passos com java

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+
+    // Primeiros passos para rodar um projeto java;
+    // dentro do parenteses, em vez de ser (String[] args), é possivel utilizar (String... args), ou um var args de pontos
+    // dessa maneira, estamos indefinindo quantas variáveis teremos em nossos sistema!
+    // nesse caso, o "args" tbm é mais como padrão, podemos utilizar outros nomes, se quisermos.
+    public static void main(String[] args) {
+
+        // Utilizando sout, o sistema já traz o System.outprintln();
+        System.out.println("Hello word");
+    }
+}
